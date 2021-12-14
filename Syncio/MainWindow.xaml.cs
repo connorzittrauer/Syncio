1e3dc23c32ae6f444f3cf86a1aaa7b3fcd942faa
connorzittrauer <zittrauerconnor@gmail.com>
2021-12-13 19:13:03 -0600

    Working File Path Selection/FileWatcher

--- Syncio/MainWindow.xaml.cs
+++ Syncio/MainWindow.xaml.cs
@@ -23,91 +23,23 @@ namespace Syncio
     /// </summary>
     public partial class MainWindow : Window
     {
+        Monitor monitor = new Monitor();
+  
+
         public MainWindow()
         {
-            InitializeComponent();
-            InitializeWatcher();
-
+            InitializeComponent();  
         }
 
-        private void InitializeWatcher()
-        {
-            FileSystemWatcher watcher = new FileSystemWatcher(@"C:\Users\Connor\Desktop\Test");
-            watcher.NotifyFilter = NotifyFilters.Attributes
-                     | NotifyFilters.CreationTime
-                     | NotifyFilters.DirectoryName
-                     | NotifyFilters.FileName
-                     | NotifyFilters.LastAccess
-                     | NotifyFilters.LastWrite
-                     | NotifyFilters.Security
-                     | NotifyFilters.Size;
-
-            watcher.Changed += OnChanged;
-            watcher.Created += OnCreated;
-            watcher.Deleted += OnDeleted;
-            watcher.Renamed += OnRenamed;
-            watcher.Error += OnError;
-
-            watcher.Filter = "*.txt";
-            watcher.IncludeSubdirectories = true;
-            watcher.EnableRaisingEvents = true;
-            
-        }
-
-        private void OnChanged(object sender, FileSystemEventArgs e)
-        {
-            if (e.ChangeType != WatcherChangeTypes.Changed)
-            {
-                return;
-            }
-            //ListBoxLog.Items.Add($"Changed: {e.FullPath}");
-            this.Dispatcher.Invoke (() => ListBoxLog.Items.Add($"Changed: {e.FullPath}"));
-
-        }
-
-        private void OnCreated(object sender, FileSystemEventArgs e)
-        {
-            string value = $"Created: {e.FullPath}";
-            this.Dispatcher.Invoke(() => ListBoxLog.Items.Add(value));
-           
-        }
-
-        private void OnDeleted(object sender, FileSystemEventArgs e)
-        {
-            this.Dispatcher.Invoke(() => ListBoxLog.Items.Add($"Deleted: {e.FullPath}"));
-        }
-            
-            
-
-        private void OnRenamed(object sender, RenamedEventArgs e)
-        {
-            this.Dispatcher.Invoke(() => ListBoxLog.Items.Add($"Renamed:"));
-            this.Dispatcher.Invoke(() => ListBoxLog.Items.Add($"    Old: {e.OldFullPath}"));
-            this.Dispatcher.Invoke(() => ListBoxLog.Items.Add($"    New: {e.FullPath}")); 
-        }
-
-        private void OnError(object sender, ErrorEventArgs e) =>
-            PrintException(e.GetException());
-
-        private void PrintException(Exception? ex)
-        {
-            if (ex != null)
-            {
-                this.Dispatcher.Invoke(() => ListBoxLog.Items.Add($"Message: {ex.Message}")); 
-                this.Dispatcher.Invoke(() => ListBoxLog.Items.Add("Stacktrace:"));
-                this.Dispatcher.Invoke(() => ListBoxLog.Items.Add(ex.StackTrace));
-                PrintException(ex.InnerException);
-            }
-        }
+        
 
 
         private void BaseDirectoryInput_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
+
             FolderBrowserDialog dialog = new FolderBrowserDialog();
             dialog.ShowDialog();
-
-            var directory = dialog.SelectedPath;
-
+            monitor.InitializeWatcher(dialog.SelectedPath);
 
 
         }
--- Syncio/Monitor.cs
+++ Syncio/Monitor.cs
@@ -1,10 +1,91 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Text;
 
 namespace Syncio
 {
     class Monitor
     {
+        FileSystemWatcher watcher;
+        public void InitializeWatcher(String filePath)
+        {
+            //watcher = new FileSystemWatcher(@"C:\Users\Connor\Desktop\Test");
+            watcher = new FileSystemWatcher(@filePath);
+            
+            watcher.NotifyFilter = NotifyFilters.Attributes
+                     | NotifyFilters.CreationTime
+                     | NotifyFilters.DirectoryName
+                     | NotifyFilters.FileName
+                     | NotifyFilters.LastAccess
+                     | NotifyFilters.LastWrite
+                     | NotifyFilters.Security
+                     | NotifyFilters.Size;
+
+            watcher.Changed += OnChanged;
+            watcher.Created += OnCreated;
+            watcher.Deleted += OnDeleted;
+            watcher.Renamed += OnRenamed;
+            //watcher.Error += OnError;
+
+            watcher.Filter = "*.txt";
+            watcher.IncludeSubdirectories = true;
+            watcher.EnableRaisingEvents = true;
+
+        }
+
+        private void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            if (e.ChangeType != WatcherChangeTypes.Changed)
+            {
+                return;
+            }
+            var log = $"Changed: {e.FullPath}";
+
+            Debug.WriteLine(log);
+
+        }
+
+        private void OnCreated(object sender, FileSystemEventArgs e)
+        {
+            var log = $"Created: {e.FullPath}";
+            Debug.WriteLine(log);
+
+        }
+
+        private void OnDeleted(object sender, FileSystemEventArgs e)
+        {
+            //this.Dispatcher.Invoke(() => ListBoxLog.Items.Add($"Deleted: {e.FullPath}"));
+            var log = $"Deleted: {e.FullPath}";
+            Debug.WriteLine(log);
+        }
+
+
+
+        private void OnRenamed(object sender, RenamedEventArgs e)
+        {
+            //this.Dispatcher.Invoke(() => ListBoxLog.Items.Add($"Renamed:"));
+            //this.Dispatcher.Invoke(() => ListBoxLog.Items.Add($"    Old: {e.OldFullPath}"));
+            //this.Dispatcher.Invoke(() => ListBoxLog.Items.Add($"    New: {e.FullPath}"));
+
+            Debug.WriteLine($"Renamed:");
+            Debug.WriteLine($"Old: {e.OldFullPath}");
+            Debug.WriteLine($"New: {e.FullPath}");
+        }
+
+        //private void OnError(object sender, ErrorEventArgs e) =>
+        //    PrintException(e.GetException());
+
+        private void PrintException(Exception? ex)
+        {
+            if (ex != null)
+            {
+                Debug.WriteLine($"Message: {ex.Message}");
+                Debug.WriteLine("Stacktrace:" + ex.StackTrace);
+                Debug.WriteLine("Inner Exception: " + ex.InnerException);
+               
+            }
+        }
     }
 }
